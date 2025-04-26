a17c96fed946154a376706cea313eafea153dafd
hattrickHL <devfullstackhugo@gmail.com>
2025-04-26 15:16:46 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,24 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //String concatenacao = "?";
+        //concatenacao = 1 + 1 + 1 + "1";
+        //System.out.println(concatenacao);
+        //concatenacao = 1 + "1" + 1 + 1;
+        //System.out.println(concatenacao);
+        //concatenacao = 1 + "1" + 1 + "1";
+        //System.out.println(concatenacao);
+        //concatenacao =  "1" + 1 + 1 + 1;
+        //System.out.println(concatenacao);
+        //concatenacao =  "1" + (1 + 1 + 1);
+        //System.out.println(concatenacao);      
+
+        int numero = 5;
+
+        numero = numero * -1;
+
+        System.out.println(- numero);
+
+        System.out.println(numero);
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo= 2500.55;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+         System.out.println(numero);
     }
+   
 }
